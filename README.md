d4c327274f64236d0a97864e9d94d8efb3f18cc2
cantputgreekletters <cantputgreekletters@users.noreply.github.com>
2023-07-15 20:15:01 +0300

    Added BubbleSort.
    -Added BubbleSort.
    
    -Made some changes.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 [The algorithm](https://www.geeksforgeeks.org/selection-sort/) repeatedly selects the smallest (or largest) element from the unsorted portion of the list and swaps it with the first element of the unsorted part. This process is repeated for the remaining unsorted portion until the entire list is sorted. 
     
 
+# Bubble Sort
+
+[Bubble Sort](https://www.geeksforgeeks.org/bubble-sort/) is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
--- Visualization.py
+++ Visualization.py
@@ -1,13 +1,14 @@
 #imports
 import pygame
 from random import randint as rn
-from time import sleep
+
 #Constants
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-idx_start = -1
+GAP = 2
+idx_start = 1 - GAP
 #classes
 class line:
     def __init__(self,x,y) -> None:
@@ -28,7 +29,7 @@ class line:
 #functions
 def make_lines():
     lines = []
-    for x in range(1,SCREEN_WIDTH + 1,2):
+    for x in range(1,SCREEN_WIDTH + 1,GAP):
         lines.append(line(x,rn(1,SCREEN_HEIGHT)))
     return lines
 
@@ -42,21 +43,37 @@ def GetLine(x):
             return line
     raise Exception("Did not return a line")
 
-def SortLines():
-    print(idx_start)
+def Swap(current,other):
+    x = current.x
+    current.x = other.x
+    other.x = x
+
+def SelectionSort():
     current_line = GetLine(idx_start)
     min_line = current_line
-    print(f"first curret line {current_line}")
-    for x in range(idx_start + 2,SCREEN_WIDTH + 1, 2):
+    
+    for x in range(idx_start + GAP,SCREEN_WIDTH + 1, GAP):
         line = GetLine(x)
         if line.height < min_line.height and line.x > min_line.x:
             min_line = line
-    x = current_line.x
-    current_line.x = min_line.x
-    min_line.x = x
-    print(f'new current line {current_line}')
+    Swap(current_line,min_line)
 
-def mainloop():
+
+#Doesnt work
+def BubbleSort():
+    for x in range(1,SCREEN_WIDTH-idx_start,GAP):
+        if x + GAP > SCREEN_WIDTH:
+            print("broke")
+            break
+        print(f'x = {x}')
+        CurrentLine = GetLine(x)
+        NextLine = GetLine(x + GAP)
+        if CurrentLine.height > NextLine.height: 
+            Swap(CurrentLine,NextLine)            
+            
+    
+
+def mainloop(SortingAlgorithm = SelectionSort):
     global RUNNING,idx_start
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,11 +84,12 @@ def mainloop():
     
     RenderLines()
     if idx_start < SCREEN_WIDTH:
-        idx_start += 2
+        idx_start += GAP
 
     if idx_start < SCREEN_WIDTH: 
-        
-        SortLines()
+        print(f"idx_start = {idx_start}")
+        SortingAlgorithm()
+    
     pygame.display.flip()
     CLOCK.tick(60)
 #main
@@ -79,5 +97,5 @@ pygame.init()
 CLOCK = pygame.time.Clock()
 RUNNING = True
 LINES = make_lines()
-while RUNNING: mainloop()
+while RUNNING: mainloop(BubbleSort)
 pygame.quit()

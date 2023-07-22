a33908dca3a9e1736625b5acfc68a7946db1833a
cantputgreekletters <cantputgreekletters@users.noreply.github.com>
2023-07-22 14:33:55 +0300

    Added gui

--- Visualization.py
+++ Visualization.py
@@ -8,8 +8,23 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 GAP = 2
-idx_start = 1 - GAP
 #classes
+class Button:
+    def __init__(self,text,x=SCREEN_WIDTH/2+50,y=SCREEN_HEIGHT/2):
+        color = (255,255,255)
+        smallfont = pygame.font.SysFont('Corbel',35)
+        self.text = text
+        text = smallfont.render(text , True , color)
+        SCREEN.blit(text , (x,y))
+        self.x,self.y = x,y
+
+    def ButtonRangeX(self):
+        Len = 35 * len(self.text) * .5
+        return range(int(self.x), int(self.x + Len))
+    
+    def ButtonRangeY(self):
+        return range(int(self.y),int(self.y+35))    
+        
 class line:
     def __init__(self,x,y) -> None:
         self.x = x
@@ -27,6 +42,7 @@ class line:
         return f'{start_pos} -> {end_pos}'
 
 #functions
+
 def make_lines():
     lines = []
     for x in range(1,SCREEN_WIDTH + 1,GAP):
@@ -71,31 +87,62 @@ def BubbleSort():
         if CurrentLine.height > NextLine.height: 
             Swap(CurrentLine,NextLine)            
             
-    
+def mainmenu():
+    global algorithm
+    while True:
+        SCREEN.fill('black')
+        MOUSE = pygame.mouse.get_pos()
+        Ss = Button('Selection Sort',SCREEN_WIDTH / 2 - 10,SCREEN_HEIGHT / 2 - 50)
+        Bs = Button('Bubble Sort',SCREEN_WIDTH / 2 - 10,SCREEN_HEIGHT / 2 + 50)
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if MOUSE[0] in Ss.ButtonRangeX() and MOUSE[1] in Ss.ButtonRangeY(): 
+                    algorithm = SelectionSort
+                    print("entered mainloop")
+                    mainloop(SelectionSort)
+                elif MOUSE[0] in Bs.ButtonRangeX() and MOUSE[1] in Bs.ButtonRangeY(): 
+                    algorithm = BubbleSort
+                    print("entered mainloop")
+                    mainloop(BubbleSort)
+
+        pygame.display.flip()
+        CLOCK.tick(60)
 
 def mainloop(SortingAlgorithm = SelectionSort):
-    global RUNNING,idx_start
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
+    RUNNING = True
+    global idx_start,LINES
+    idx_start = 1 - GAP
+    LINES = make_lines()
+    while RUNNING:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
 
-    
-    SCREEN.fill("black")
-    
-    RenderLines()
-    if idx_start < SCREEN_WIDTH:
-        idx_start += GAP
+        
+        SCREEN.fill("black")
+        
+        RenderLines()
+        if idx_start < SCREEN_WIDTH:
+            idx_start += GAP
 
-    if idx_start < SCREEN_WIDTH: 
-        print(f"idx_start = {idx_start}")
-        SortingAlgorithm()
-    
-    pygame.display.flip()
-    CLOCK.tick(60)
+        if idx_start < SCREEN_WIDTH: 
+            print(f"idx_start = {idx_start}")
+            SortingAlgorithm()
+        
+        pygame.display.flip()
+        CLOCK.tick(60)
+
+
+
 #main
+global LINES
 pygame.init()
 CLOCK = pygame.time.Clock()
-RUNNING = True
-LINES = make_lines()
-while RUNNING: mainloop(BubbleSort)
+
+
+mainmenu()
+
 pygame.quit()
